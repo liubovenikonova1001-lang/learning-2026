6388fc88eae89ce82a178c6ccbf3a3c8c3832fa3
Liubovenikonova1001-lang <liubovenikonova1001-lang@users.noroply.github.com>
2026-09-04 11:04:28 +0300

    День 1: переменные и типы данных

---
+++ day1.js
@@ -0,0 +1,13 @@
+let name = "Анна";
+let aage = 25;
+let isStudent = true;
+let hobbies = ["чтение","код","музыка"];
+let user = {
+    city:"Москва",
+    country: "Россия"
+};
+console.log(`Имя: ${name}`);
+console.log(`Возраст: ${age}`);
+console.log(`Студентка? ${isStudent}`);
+console.log(`Хобби: ${hobbies}`);
+console.log(`Город: ${user.city}`);
